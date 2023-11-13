bbf7717667ad610d90056cb3c52d2156e7fbcdce
Serpil Özgüven <serpilozguven776@gmail.com>
2023-11-13 17:26:24 +0300

    initial commit

---
+++ lib/line_icons.dart
@@ -0,0 +1,19 @@
+
+class LineIconsPage extends StatefulWidget {
+  const LineIconsPage({super.key});
+
+  @override
+  State<LineIconsPage> createState() => _LineIconsPageState();
+}
+
+class _LineIconsPageState extends State<LineIconsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Line Icons'),
+      ),
+      body: Container(),
+    );
+  }
+}
